b0c2a384e60434c7933bcff59420a5e43fe58b2d
Daniel Han <ghan@student.42seoul.kr>
2025-03-15 17:25:32 +0900

    FEAT: set global validation pipe

--- patient-service/src/main.ts
+++ patient-service/src/main.ts
@@ -2,8 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getApiDocumentConfig } from './getApiDocumentConfig';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   // Set up swagger
   const apiDocConfig = getApiDocumentConfig();
