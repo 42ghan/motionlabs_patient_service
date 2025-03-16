cfdc30ed6405ef8e7b178a396b40101f6db4fb9c
Daniel Han <ghan@student.42seoul.kr>
2025-03-16 17:01:44 +0900

    ENH: throw error when MYSQL_URL is not in .env

--- patient-service/src/config-factory/config-factory.service.ts
+++ patient-service/src/config-factory/config-factory.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Patient } from 'src/patients/entities/patient.entity';
+import { Patient } from '../patients/entities/patient.entity';
 
 @Injectable()
 export class ConfigFactoryService {
   constructor(private readonly configService: ConfigService) {}
 
   get mySqlConfig(): TypeOrmModuleOptions {
+    if (!this.configService.get('MYSQL_URL')) {
+      throw new Error('MYSQL_URL is not set');
+    }
     return {
       type: 'mysql',
       url: this.configService.get('MYSQL_URL'),
